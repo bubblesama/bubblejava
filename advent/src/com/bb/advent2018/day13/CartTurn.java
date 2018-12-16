f9417099b61009a0e4e8fe7e66db558df99c8c23
Unknown <paul.remignon@gmail.com>
2018-12-17 00:11:31 +0100

    advent2018: day13 part1 switch directions

--- advent/src/com/bb/advent2018/day13/CartTurn.java
+++ advent/src/com/bb/advent2018/day13/CartTurn.java
@@ -2,7 +2,25 @@ package com.bb.advent2018.day13;
 
 public enum CartTurn {
 
+	LEFT,RIGHT,STRAIGHT;
 	
-	LEFT,RIGHT,STRAIGHT
+	public CartTurn next() {
+		CartTurn result = CartTurn.STRAIGHT;
+		switch (this) {
+		case LEFT:
+			result = CartTurn.STRAIGHT;
+			break;
+		case STRAIGHT:
+			result = CartTurn.RIGHT;
+			break;
+		case RIGHT:
+			result = CartTurn.LEFT;
+			break;
+		default:
+			break;
+		}
+		return result;
+		
+	}
 	
 }
