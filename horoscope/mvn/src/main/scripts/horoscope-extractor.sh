d634b7adc49edbc8b0de09c49985ba357877205b
Unknown <paul.remignon@gmail.com>
2020-06-21 14:29:44 +0200

    horoscope: shell update for new version

--- horoscope/mvn/src/main/scripts/horoscope-extractor.sh
+++ horoscope/mvn/src/main/scripts/horoscope-extractor.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # script to launch the horoscope getter then delete temporary pdf files
-cd /projects/horoscope/
-java -classpath /projects/horoscope/extractor-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.bbtools.horos.extractor.HorosPdfGetter >> /projects/horoscope/logs/horoscope.log
+cd /projects/horoscope/batch/
+java -classpath /projects/horoscope/batch/horoscope-extractor-1.0.1-SNAPSHOT-jar-with-dependencies.jar com.bbtools.horos.extractor.HorosPdfGetter >> /projects/horoscope/logs/horoscope.log
 rm /projects/horoscope/data/*.pdf
